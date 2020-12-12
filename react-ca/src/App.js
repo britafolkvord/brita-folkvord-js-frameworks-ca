cd68d2a051e3cf422b3d76403997d801e3bb2192
britafolkvord <brita.folk@hotmail.com>
2020-12-12 22:47:55 +0100

    first

---
+++ react-ca/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Home from './components/home/home';
+import Layout from './components/layout/layout';
+
+function App() {
+  return (
+    <Layout>
+    <Home/>
+    </Layout>
+  );
+}
+
+export default App;
---
+++ react-ca/src/components/contact/errorMessage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function ErrorMessage(props) {
+    return (
+        <div className="error">{props.children}</div>
+    );
+}
+
+export default ErrorMessage;
